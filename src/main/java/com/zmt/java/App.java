62cad60a909c35e1f2ceb9771e094ad147f509da
zhumengting <444638290@qq.com>
2019-11-25 20:35:04 +0800

    test

---
+++ src/main/java/com/zmt/java/App.java
@@ -0,0 +1,14 @@
+package com.zmt.java;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
