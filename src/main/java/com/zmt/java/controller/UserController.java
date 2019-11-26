3b52c5e20a0823ef561b0176dfa08f64afe91d82
zhumengting <444638290@qq.com>
2019-11-26 17:48:21 +0800

    修改配置

---
+++ src/main/java/com/zmt/java/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.zmt.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World !!!";
+    }
+
+    @RequestMapping("/hello2")
+    @ResponseBody
+    public String hello2() {
+        return "Hello mt !!!";
+    }
+
+}
